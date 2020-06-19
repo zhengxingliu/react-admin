5149b46a94c3e718781a047091250297e0c2785a
zhengxingliu <45699390+zhengxingliu@users.noreply.github.com>
2020-06-19 01:51:29 -0400

    add less support

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,13 @@
  */
 
 
-const { override } = require("customize-cra");
+const { override, addLessLoader } = require("customize-cra");
 
 module.exports = override(
- 
+ addLessLoader({
+  lessOptions: {
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' }
+  }
+ })
 );
