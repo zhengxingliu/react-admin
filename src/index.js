4e09736a41e86d772419d47805c4912617404010
zhengxingliu <45699390+zhengxingliu@users.noreply.github.com>
2020-08-26 21:35:13 -0400

    add github page config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 
