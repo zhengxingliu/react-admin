dbb10accf50e46840644b01b174a261a780a2074
zhengxingliu <45699390+zhengxingliu@users.noreply.github.com>
2020-08-26 21:17:37 -0400

    Updates

---
+++ precache-manifest.296a5ee955f5476e78d22b7f5853a3d0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dccf98004fd34bb02c4fa802cafbc86",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "ae54d60d084ff5f42c6b",
+    "url": "/react-admin/static/css/0.3a604514.chunk.css"
+  },
+  {
+    "revision": "7c89a866f47690e630aa",
+    "url": "/react-admin/static/css/1.d2dcc60a.chunk.css"
+  },
+  {
+    "revision": "2512c24eb735c8702631",
+    "url": "/react-admin/static/css/10.3073f373.chunk.css"
+  },
+  {
+    "revision": "2208c68358b6500e17db",
+    "url": "/react-admin/static/css/11.0802aed6.chunk.css"
+  },
+  {
+    "revision": "ec2dfb42979216cd901a",
+    "url": "/react-admin/static/css/12.d75714de.chunk.css"
+  },
+  {
+    "revision": "33015034cab0a723ec0d",
+    "url": "/react-admin/static/css/6.73461c1a.chunk.css"
+  },
+  {
+    "revision": "88a538193f94cd1df835",
+    "url": "/react-admin/static/css/7.ccf58c9a.chunk.css"
+  },
+  {
+    "revision": "7a18d43ad12732be4ab6",
+    "url": "/react-admin/static/css/8.13608ff0.chunk.css"
+  },
+  {
+    "revision": "2fcd205f6dd2c7ba3a69",
+    "url": "/react-admin/static/css/9.8a4ac6a0.chunk.css"
+  },
+  {
+    "revision": "eeacb714ec9df209898a",
+    "url": "/react-admin/static/css/main.930f8d01.chunk.css"
+  },
+  {
+    "revision": "ae54d60d084ff5f42c6b",
+    "url": "/react-admin/static/js/0.e15a587a.chunk.js"
+  },
+  {
+    "revision": "7c89a866f47690e630aa",
+    "url": "/react-admin/static/js/1.bd5d578d.chunk.js"
+  },
+  {
+    "revision": "2512c24eb735c8702631",
+    "url": "/react-admin/static/js/10.744a6ad1.chunk.js"
+  },
+  {
+    "revision": "31dba32ae1896cd5a50ff8aac5c24fec",
+    "url": "/react-admin/static/js/10.744a6ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2208c68358b6500e17db",
+    "url": "/react-admin/static/js/11.2ba1376d.chunk.js"
+  },
+  {
+    "revision": "ec2dfb42979216cd901a",
+    "url": "/react-admin/static/js/12.67cee465.chunk.js"
+  },
+  {
+    "revision": "52404f7199e28965c7a0",
+    "url": "/react-admin/static/js/13.fd086f0b.chunk.js"
+  },
+  {
+    "revision": "42ca3b370fbdee835e2a",
+    "url": "/react-admin/static/js/14.81d594e2.chunk.js"
+  },
+  {
+    "revision": "a19a22a5f73517dbf10d",
+    "url": "/react-admin/static/js/15.e8b2c281.chunk.js"
+  },
+  {
+    "revision": "a8d2d8bde485f84139c1",
+    "url": "/react-admin/static/js/16.bec6b273.chunk.js"
+  },
+  {
+    "revision": "6b1ff604bf0647fbce79",
+    "url": "/react-admin/static/js/17.9397cc77.chunk.js"
+  },
+  {
+    "revision": "8c046893d55002266e50",
+    "url": "/react-admin/static/js/18.0d260fd5.chunk.js"
+  },
+  {
+    "revision": "2ee1605eea3959fcc4f0",
+    "url": "/react-admin/static/js/2.38cba2cf.chunk.js"
+  },
+  {
+    "revision": "30d2ba8d504ba34adc5a",
+    "url": "/react-admin/static/js/3.21b99310.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/react-admin/static/js/3.21b99310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33015034cab0a723ec0d",
+    "url": "/react-admin/static/js/6.554367e5.chunk.js"
+  },
+  {
+    "revision": "88a538193f94cd1df835",
+    "url": "/react-admin/static/js/7.e0aa7a02.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-admin/static/js/7.e0aa7a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a18d43ad12732be4ab6",
+    "url": "/react-admin/static/js/8.dc3205d9.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/react-admin/static/js/8.dc3205d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcd205f6dd2c7ba3a69",
+    "url": "/react-admin/static/js/9.09d8ccc9.chunk.js"
+  },
+  {
+    "revision": "eeacb714ec9df209898a",
+    "url": "/react-admin/static/js/main.cbd27426.chunk.js"
+  },
+  {
+    "revision": "d6893c44c95abe88781d",
+    "url": "/react-admin/static/js/runtime-main.4b885695.js"
+  },
+  {
+    "revision": "b14db446cc15c99080e4530768cb09cb",
+    "url": "/react-admin/static/media/AdminLogo.b14db446.png"
+  }
+]);
